cd948b1426d69c0ea6adf4c66044f2d97e871e6c
Maxime Perraud <contact@maxime-p.dev>
2024-04-22 14:00:02 +0200

    init

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import matomo from "@jop-software/astro-matomo";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://maxime-p.github.io",
+  base: "/portfolio",
+  integrations: [matomo({
+    baseUrl: "https://analytics.maxime-p.dev",
+    siteId: 1
+  })]
+});
---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export const userData = {
+  username: 'Nicolas Lapointe',
+  description:
+    'Practice Leader DevWeb\n' + '\n' + 'Clean architecture addict 🫶',
+  formations: ['5567130'],
+  publications: ['c713a3a525cc'],
+}
